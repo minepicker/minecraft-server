d62a45a04b203fac99437848d2c3d661ec8bddaf
Minepicker <contact@minepicker.com>
2025-04-01 19:19:56 +0100

    1.13.0

--- index.d.ts
+++ index.d.ts
@@ -15,7 +15,7 @@
  * ```json
  * {
  *   "module_name": "@minecraft/server",
- *   "version": "1.12.0"
+ *   "version": "1.13.0"
  * }
  * ```
  *
@@ -530,6 +530,13 @@ export enum EntityComponentTypes {
      *
      */
     LavaMovement = 'minecraft:lava_movement',
+    /**
+     * @remarks
+     * Allows this entity to be leashed and defines the conditions
+     * and events for this entity when is leashed.
+     *
+     */
+    Leashable = 'minecraft:leashable',
     /**
      * @remarks
      * When added, this component signifies that this entity
@@ -4227,6 +4234,13 @@ export class Dimension {
     getPlayers(options?: EntityQueryOptions): Player[];
     /**
      * @remarks
+     * This function can't be called in read-only mode.
+     *
+     * @throws This function can throw errors.
+     */
+    getTopmostBlock(locationXZ: VectorXZ, minHeight?: number): Block | undefined;
+    /**
+     * @remarks
      * Plays a sound for all players.
      *
      * This function can't be called in read-only mode.
@@ -6732,6 +6746,91 @@ export class EntityItemComponent extends EntityComponent {
 export class EntityLavaMovementComponent extends EntityAttributeComponent {
     private constructor();
     static readonly componentId = 'minecraft:lava_movement';
+}
+
+/**
+ * Allows the entity to be leashed. Defines the conditions and
+ * events for when an entity is leashed.
+ */
+// @ts-ignore Class inheritance allowed for native defined classes
+export class EntityLeashableComponent extends EntityComponent {
+    private constructor();
+    /**
+     * @remarks
+     * Returns true if another entity can 'steal' the leashed
+     * entity by attaching their own leash to it.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly canBeStolen: boolean;
+    /**
+     * @remarks
+     * Distance in blocks at which the leash stiffens, restricting
+     * movement.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly hardDistance: number;
+    /**
+     * @remarks
+     * Returns true if the entity is leashed.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly isLeashed: boolean;
+    /**
+     * @remarks
+     * Entity that is holding the leash.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly leashHolder?: Entity;
+    /**
+     * @remarks
+     * Identifier of entity that is holding the leash.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly leashHolderEntityId?: string;
+    /**
+     * @remarks
+     * Distance in blocks at which the leash breaks.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly maxDistance: number;
+    /**
+     * @remarks
+     * Distance in blocks at which the 'spring' effect starts
+     * acting to keep this entity close to the entity that leashed
+     * it.
+     *
+     * @throws This property can throw when used.
+     */
+    readonly softDistance: number;
+    static readonly componentId = 'minecraft:leashable';
+    /**
+     * @remarks
+     * Leashes this entity to another entity.
+     *
+     * This function can't be called in read-only mode.
+     *
+     * @param leashHolder
+     * The entity to leash this entity to.
+     * @throws
+     * Throws if the entity to leash to is over the max distance,
+     * and if the player is dead or in spectator mode.
+     */
+    leashTo(leashHolder: Entity): void;
+    /**
+     * @remarks
+     * Unleashes this entity if it is leashed to another entity.
+     *
+     * This function can't be called in read-only mode.
+     *
+     * @throws This function can throw errors.
+     */
+    unleash(): void;
 }
 
 /**
@@ -15539,6 +15638,11 @@ export interface Vector3 {
      * Z component of this vector.
      *
      */
+    z: number;
+}
+
+export interface VectorXZ {
+    x: number;
     z: number;
 }
 
